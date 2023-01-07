46c402c3ef672145d744a739210baaf92a9f88b7
LeMajestie <cristhian_s94@outlook.com>
2023-01-07 18:04:39 -0500

    First Commit

---
+++ BatteryPlugged.bat
@@ -0,0 +1,9 @@
+@echo off
+
+start [controlcenter.exe]
+
+exit
+
+git config --global user.name "LeMajestie"
+
+git config --global user.email "cristhian_s94@outlook.com"
---
+++ BatteryUnplugged.bat
@@ -0,0 +1,5 @@
+@echo off
+
+taskkill/im controlcenter.exe /f
+
+exit
